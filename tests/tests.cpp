54e802b0e739e78a97634ae802cd56eb19ec1de7
papazloynt <cool.chastikov53@gmail.com>
2021-10-14 13:58:53 +0300

    add test

---
+++ tests/tests.cpp
@@ -0,0 +1,72 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "date.h"
+#include <malloc.h>
+}
+
+TEST(date, wrong_size) {
+    ASSERT_EQ(fillingDates(-1), nullptr);
+}
+
+TEST(date, wrong_dates) {
+    const int size = 3;
+    const int wrong_number = 40;
+
+    Date* dates = (Date*)malloc(size * sizeof(Date));
+    if (dates == NULL) FAIL();
+
+    for (size_t i = 0; i < size; ++i) {
+        dates[i].day_number = wrong_number;
+        dates[i].month = wrong_number;
+        dates[i].year = 2021;
+    }
+    ASSERT_EQ(isCorrectDates(dates, size), false);
+
+    free(dates);
+}
+
+
+TEST(date, check_sort) {
+    const int size = 3;
+
+    Date* dates = (Date*)malloc(size * sizeof(Date));
+    if (dates == NULL) FAIL();
+
+    dates[0].day_number = 31;
+    dates[0].month = 10;
+    dates[0].year = 2000;
+
+    dates[1].day_number = 2;
+    dates[1].month = 11;
+    dates[1].year = 2021;
+
+    dates[2].day_number = 2;
+    dates[2].month = 10;
+    dates[2].year = 2000;
+
+    ASSERT_EQ(isCorrectDates(dates, size), true);
+
+    sortDates(dates, size);
+
+    ASSERT_EQ(dates[0].day_number, 2);
+    ASSERT_EQ(dates[0].month, 10);
+    ASSERT_EQ(dates[0].year, 2000);
+
+    ASSERT_EQ(dates[1].day_number, 31);
+    ASSERT_EQ(dates[1].month, 10);
+    ASSERT_EQ(dates[1].year, 2000);
+
+    ASSERT_EQ(dates[2].day_number, 2);
+    ASSERT_EQ(dates[2].month, 11);
+    ASSERT_EQ(dates[2].year, 2021);
+
+    free(dates);
+}
+
+
+
+int main(int argc, char* argv[]){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
